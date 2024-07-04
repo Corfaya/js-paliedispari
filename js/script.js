efae2c782684c30f8cbbb4e0e3759a8a7bc90712
Corfaya <lauracorrado0696@gmail.com>
2024-07-04 16:20:41 +0200

    [UPD] while loop to check userSelection's value is "pari" or "dispari"

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// ========== FIRST EXERCISE ==========
-// DICHIARAZIONE DELLA FUNZIONE
-function palindrome(word) {
-    // With split("") method, create an array with single letters. With reverse() methos, reverse the order of the array elements. With join("") method, combine the elements into a string, without commas
-    let reverseWord = word.split("").reverse().join("");
-    // If word's value is the same of reverseWord's value, the word is a palindrome
-    if (word == reverseWord) {
-        console.log(`La parola "${word}" è palindroma. Letta al contrario è "${reverseWord}".`)
-    } else {
-        console.log(`La parola "${word}" non è palindroma. Letta al contrario è "${reverseWord}".`)
-    }
-}
-// variables for user input
-let userWord = prompt("Inserisci una parola")
-// Function invocation with userWord as a parameter
-palindrome(userWord)
+// // ========== FIRST EXERCISE ==========
+// // DICHIARAZIONE DELLA FUNZIONE
+// function palindrome(word) {
+//     // With split("") method, create an array with single letters. With reverse() methos, reverse the order of the array elements. With join("") method, combine the elements into a string, without commas
+//     let reverseWord = word.split("").reverse().join("");
+//     // If word's value is the same of reverseWord's value, the word is a palindrome
+//     if (word == reverseWord) {
+//         console.log(`La parola "${word}" è palindroma. Letta al contrario è "${reverseWord}".`)
+//     } else {
+//         console.log(`La parola "${word}" non è palindroma. Letta al contrario è "${reverseWord}".`)
+//     }
+// }
+// // variables for user input
+// let userWord = prompt("Inserisci una parola")
+// // Function invocation with userWord as a parameter
+// palindrome(userWord)
 
 // ========== SECOND EXERCISE ==========
 // DICHIARAZIONE DELLE FUNZIONI
@@ -25,6 +25,14 @@ function even(num) {
 }
 // even or odd variable
 let userSelection = prompt("Pari o dispari?").toLowerCase()
+// CHECK userSelection's value
+while (userSelection !== "pari" || userSelection !== "dispari") {
+    userSelection = prompt("Errore. Scrivi 'pari' o 'dispari'").toLowerCase()
+    if (userSelection == "pari" || userSelection == "dispari") {
+        break
+    }
+}
+console.log(userSelection)
 // 1-5 number variable
 let userNum = parseInt(prompt("Scegli un numero da 1 a 5"))
 // pc random number has to be saved in a variable

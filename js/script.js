839a08d3e04ad695d057562ed994ebd67d2df43a
Corfaya <lauracorrado0696@gmail.com>
2024-07-04 14:10:58 +0200

    [UPD] Function invocation with user parameter. EXERCISE 1 DONE

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ function palindrome(word) {
     let reverseWord = word.split("").reverse().join("");
     // If word's value is the same of reverseWord's value, the word is a palindrome
     if (word == reverseWord) {
-        console.log(`La parola "${word}" è palindroma. Letta al contrario è "${reverseWord}"`)
+        console.log(`La parola "${word}" è palindroma. Letta al contrario è "${reverseWord}".`)
     } else {
-        console.log(`La parola "${word}" non è palindroma. Letta al contrario è "${reverseWord}"`)
+        console.log(`La parola "${word}" non è palindroma. Letta al contrario è "${reverseWord}".`)
     }
 }
 // variables for user input
 let userWord = prompt("Inserisci una parola")
+// Function invocation with userWord as a parameter
+palindrome(userWord)

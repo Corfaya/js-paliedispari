91eb26752dac152972ad502a947e2166f54a6837
Corfaya <lauracorrado0696@gmail.com>
2024-07-04 14:05:20 +0200

    [UPD] if statement added, checking if the input word is the same of the reversed word

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@
 function palindrome(word) {
     // With split("") method, create an array with single letters. With reverse() methos, reverse the order of the array elements. With join("") method, combine the elements into a string, without commas
     let reverseWord = word.split("").reverse().join("");
+    // If word's value is the same of reverseWord's value, the word is a palindrome
+    if (word == reverseWord) {
+        console.log(`La parola "$(word)" è palindroma. Letta al contrario è "${reverseWord}"`)
+    } else {
+
+    }
 }
